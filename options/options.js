eb54d28a9d2911fd1f9f6533291679664501462e
Crowdbotics <engineering@crowdbotics.com>
2023-03-17 18:27:37 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "weathered-wood-6",
+  url: "https://weathered-wood-6.botics.co",
+  api: "https://weathered-wood-6.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
